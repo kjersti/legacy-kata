b5dbeb4c3c1a571882efd7431057eaad3b26b008
Kjersti Berg <kjersti.berg@scaleaq.com>
2021-09-30 13:25:49 +0200

    Add verify-prod tool

---
+++ verify-prod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace verify_prod
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var output = File.ReadAllLines("output.txt", Encoding.UTF8);
+            var expected = File.ReadAllLines("expected.txt", Encoding.UTF8);
+            var verified = expected.SequenceEqual(output);
+            if (verified)
+            {
+                Console.WriteLine("Looking good!");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("Expected:");
+                Console.Write(string.Join(',', expected));
+                Console.WriteLine("Was:");
+                Console.Write(string.Join(',', output));
+                return 1;
+            }
+        }
+    }
+}
