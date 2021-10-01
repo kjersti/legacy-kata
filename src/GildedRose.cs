7a6479c988ae756aed54518f590f2eabfee5529c
Aleksander <aleksander.jozefiak@scaleaq.com>
2021-10-01 10:30:01 +0200

    Feature toggle for conjured item

--- src/GildedRose.cs
+++ src/GildedRose.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace GildedRoseKata
 {
     public class GildedRose
     {
+        public bool ConjuredEnabled = false;
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
@@ -14,9 +16,9 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name == "Conjured Mana Cake")
+                if (ConjuredEnabled && Items[i].Name == "Conjured Mana Cake")
                 {
-                    Items[i].Quality = Items[i].Quality - 2;
+                    Items[i].Quality = Math.Max(0, Items[i].Quality - 2);
                 }
                 else if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[i].Name != "Regular ticket to a TAFKAL80ETC concert")
                 {
