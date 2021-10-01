b651f92b0055ab127c43c45051a44de382281135
Aleksander <aleksander.jozefiak@scaleaq.com>
2021-10-01 10:13:44 +0200

    Conjured item test

---
+++ tests/ConjuredItemTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using Xunit;
+
+namespace tests
+{
+    public class ConjuredItemTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var list = GenerateItems();
+            
+            Assert.True(true);
+        }
+
+        private IList<Item> GenerateItems()
+        {
+             IList<Item> items = new List<Item>{
+				// this conjured item does not work properly yet
+				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+             return items;
+        }
+    }
+}
