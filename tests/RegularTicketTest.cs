1ab9aa6d40204f6eba47adeb917ac55aab5c100f
TinkLee <26019988+TinkLee@users.noreply.github.com>
2021-10-01 16:08:01 +0700

    Add UT for Regular Ticket

---
+++ tests/RegularTicketTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GildedRoseKata;
+using Xunit;
+
+namespace tests
+{
+    public class RegularTicketTest
+    {
+        [Fact]
+        public void QualityShouldIncreaseByDayTest()
+        {
+            IList<Item> items = new List<Item>{
+				new Item {
+                    Name = "Regular ticket to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 20
+                }
+            };
+            var gildedRose = new GildedRose(items);
+            gildedRose.ConjuredEnabled = true;
+            Assert.Equal(20, items.First().Quality);
+            gildedRose.UpdateQuality();
+            Assert.Equal(21, items.First().Quality);
+        }
+
+        [Fact]
+        public void QualityShouldBeZeroWhenSellInTest()
+        {
+            IList<Item> items = new List<Item>{
+				new Item {
+                    Name = "Regular ticket to a TAFKAL80ETC concert",
+                    SellIn = 1,
+                    Quality = 20
+                }
+            };
+            var gildedRose = new GildedRose(items);
+            gildedRose.ConjuredEnabled = true;
+            Assert.Equal(20, items.First().Quality);
+            gildedRose.UpdateQuality();
+            gildedRose.UpdateQuality();
+            Assert.Equal(0, items.First().Quality);
+        }
+
+    }
+}
